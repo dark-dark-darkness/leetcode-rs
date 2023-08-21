c75ea37daf34f7469bcbb784af760614702d36ae
DingYu Ren <572917108@qq.com>
2023-08-21 10:49:47 +0800

    add move-pieces-to-obtain-a-string

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -12,6 +12,7 @@ mod maximum_subarray;
 mod median_of_two_sorted_arrays;
 mod merge_two_binary_trees;
 mod minimum_falling_path_sum_ii;
+mod move_pieces_to_obtain_a_string;
 mod number_of_ways_of_cutting_a_pizza;
 mod pizza_with_3n_slices;
 mod power_of_heroes;
---
+++ src/problems/move_pieces_to_obtain_a_string.rs
@@ -0,0 +1,88 @@
+use crate::Solution;
+
+// https://leetcode.cn/problems/move-pieces-to-obtain-a-string
+
+impl Solution {
+    pub fn can_change(start: String, target: String) -> bool {
+        let (start, target) = (start.as_bytes(), target.as_bytes());
+        let len = if start.len() == target.len() {
+            start.len()
+        } else {
+            return false;
+        };
+
+        let (mut i, mut j) = (0, 0);
+
+        while i < len && j < len {
+            match (start[i], target[j]) {
+                (b'L', b'L') if i >= j => {
+                    i += 1;
+                    j += 1;
+                }
+                (b'R', b'R') if i <= j => {
+                    i += 1;
+                    j += 1;
+                }
+                (b'_', _) => {
+                    i += 1;
+                }
+                (_, b'_') => {
+                    j += 1;
+                }
+                _ => {
+                    return false;
+                }
+            }
+        }
+
+        while i < len && start[i] == b'_' {
+            i += 1;
+        }
+        while j < len && target[j] == b'_' {
+            j += 1;
+        }
+
+        return (i == len) && (i == j);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn case_1() {
+        let start = "_L__R__R_";
+        let target = "L______RR";
+
+        let right = true;
+
+        let result = Solution::can_change(start.to_string(), target.to_string());
+
+        assert_eq!(result, right);
+    }
+
+    #[test]
+    fn case_2() {
+        let start = "R_L_";
+        let target = "__LR";
+
+        let right = false;
+
+        let result = Solution::can_change(start.to_string(), target.to_string());
+
+        assert_eq!(result, right);
+    }
+
+    #[test]
+    fn case_3() {
+        let start = "_R";
+        let target = "R_";
+
+        let right = false;
+
+        let result = Solution::can_change(start.to_string(), target.to_string());
+
+        assert_eq!(result, right);
+    }
+}
