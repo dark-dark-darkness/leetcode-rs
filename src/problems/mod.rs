6a707c6753d938070153fd7884ab4a7d21c121db
dyren <polecat-03-eidola@icloud.com>
2024-08-18 21:46:00 +0800

    551. 学生出勤记录 I

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -68,6 +68,7 @@ mod right_triangles;
 mod rings_and_rods;
 mod serialize_and_deserialize_bst;
 mod special_array_ii;
+mod student_attendance_record_i;
 mod subtract_the_product_and_sum_of_digits_of_an_integer;
 mod three_sum;
 mod trapping_rain_water;
